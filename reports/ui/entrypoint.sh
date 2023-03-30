cd34363c2b82aa7aae91b89b150f525dc22f036c
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-03-30 12:22:36 +0200

    Added nginx configuration, organized reports building

--- reports/ui/entrypoint.sh
+++ reports/ui/entrypoint.sh
@@ -3,15 +3,11 @@
 set -e
 
 # if command is provided, execute it
-if [ -n "$1" ]; then
-  exec "$@"
-else
+if [ "$1" == 'nginx' ]; then
   # check if UI_HOST and UI_PORT are set
   if [ -z "${APP_HOST}" ] || [ -z "${APP_PORT}" ]; then
     echo "APP_HOST and APP_PORT must be set"
     exit 1
   fi
   wait-for "${APP_HOST}:${APP_PORT}" -t 30 -- echo "App is up"
-
-  exec /docker-entrypoint.sh "$@"
 fi
