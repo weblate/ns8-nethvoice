ddb563f2a5ae81c82ee7c767ca4cf6d2fc05197c
Stefano Fancello <stefano.fancello@nethesis.it>
2023-03-30 12:02:58 +0200

    Remove RTPSTART and RTPEND from Janus since are also passed as command line variables

--- janus/entrypoint.sh
+++ janus/entrypoint.sh
@@ -6,7 +6,6 @@
 #
 
 # Change SIP plugins port
-sed -i "s/\trtp_port_range = .*/\trtp_port_range = \"${RTPSTART:=10000}-${RTPEND:=20000}\"/" /usr/local/etc/janus/janus.plugin.sip.jcfg
 if [[ -z ${LOCAL_IP} ]]; then
 	sed -i "s/\t#local_ip = .*/\tlocal_ip = \"${LOCAL_IP}\"/" /usr/local/etc/janus/janus.plugin.sip.jcfg
 	sed -i "s/\t#local_media_ip = .*/\tlocal_media_ip = \"${LOCAL_IP}\"/" /usr/local/etc/janus/janus.plugin.sip.jcfg
