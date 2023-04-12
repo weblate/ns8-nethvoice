8b7781f8961d9162361c00c16ed9e3c8bf4e0bc6
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-04-12 12:43:31 +0200

    refactor: cloning nethcti-server instead of local revision

--- nethcti-server/usr/lib/node/nethcti-server/plugins/dbconn/sequelize_models/userman_users.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('userman_users', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true
-    },
-    username: DataTypes.STRING
-  });
-}
--- nethcti-server/usr/lib/node/nethcti-server/test/collectd-alarm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $# -eq 0 ]; then
-  echo "use: $0 <QUEUE_NUMBER>"
-  exit 1
-fi
-QUEUE=$1
-/usr/bin/echo -e "{\"type\":\"collectd_notify\", \"notification\": {\"type\":\"queuefewop\", \"type_instance\":\"$QUEUE\", \"status\":\"warning\", \"message\":\"foo bar\"}}" | nc -U /run/nethvoice/nethcti.sock
-echo "alarm \"queuefewop\" alarm emitted with severity \"warning\" for QUEUE $QUEUE"
