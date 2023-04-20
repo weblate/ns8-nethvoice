ccf0386aef59b0597150d7723a9201155defce59
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2023-04-20 12:14:35 +0200

    asterisk: hack for default ports
    - IAX: if left blank, asterisk will listen on port 4569
      even if IAX is not configured
    - SIP: set SIP port, asterisk will lsiten on port 5060
      even if SIP is not configured
    - Builtin mini-HTTP server: disable listen on TLS port

--- mariadb/docker-entrypoint-initdb.d/70_asterisk_ports.sh
+++ mariadb/docker-entrypoint-initdb.d/70_asterisk_ports.sh
@@ -11,6 +11,19 @@ astmanager_sql='INSERT INTO `freepbx_settings` (`keyword`,`value`,`name`,`level`
 
 pjsip_sql='INSERT INTO `kvstore_Sipsettings` (`key`,`val`,`type`,`id`) VALUES ("bindport","'${ASTERISK_SIP_PORT}'",NULL,"noid"),("tlsbindport","'${ASTERISK_SIPS_PORT}'",NULL,"noid");'
 
+# avoid listening on default 4569 port even if IAX is not configured
+iax_sql_hack='INSERT INTO `iaxsettings` (`keyword`,`data`,`seq`,`type`) VALUES ("bindport","'${ASTERISK_IAX_PORT}'",1,0)'
+
+# avoid listening on default 5060 port even if SIP is not configured
+# this will cause a warning like: Failed to bind to 0.0.0.0:32120: Address already in use
+sip_sql_hack='INSERT INTO `sipsettings` VALUES ("bindport","'${ASTERISK_SIP_PORT}'",1,0)'
+
+# do not listen on port 8089
+http_sql_hack='INSERT INTO `freepbx_settings` (`keyword`,`value`,`name`,`level`,`description`,`type`,`options`,`defaultval`,`readonly`,`hidden`,`category`,`module`,`emptyok`,`sortorder`) VALUES ("HTTPTLSENABLE","0","Enable TLS for the mini-HTTP Server",3,"Enables listening for HTTPS connections. This is for Asterisk, it is not directly related for FreePBX usage and the value of this setting is irrelevant for accessing core FreePBX settings. Default is no.","bool","","0",0,0,"Asterisk Builtin mini-HTTP server","",0,0)'
+
 /usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e "$sip_sql"
 /usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e "$pjsip_sql"
 /usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e "$astmanager_sql"
+/usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e "$sip_sql_hack"
+/usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e "$iax_sql_hack"
+/usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e "$http_sql_hack"
