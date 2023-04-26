e2db600115a906804331788b5e108baa30d3565c
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-04-26 09:30:39 +0200

    fix(reports): moved configuration file

--- reports/api/entrypoint.sh
+++ reports/api/entrypoint.sh
@@ -5,12 +5,12 @@ set -e
 # if command is api, wait for services to be up
 if [ "$1" = "api" ]; then
   # Wait for redis to be ready
-  redis_address=$(jq -r '.redis_address' /opt/nethvoice-report/api/conf.json)
+  redis_address=$(jq -r '.redis_address' /opt/nethvoice-report.json)
   wait-for "${redis_address}" -t 30 -- echo "redis is up"
 
   # Wait for cdr database to be ready
-  cdr_database_host=$(jq -r '.cdr_database.host' /opt/nethvoice-report/api/conf.json)
-  cdr_database_port=$(jq -r '.cdr_database.port' /opt/nethvoice-report/api/conf.json)
+  cdr_database_host=$(jq -r '.cdr_database.host' /opt/nethvoice-report.json)
+  cdr_database_port=$(jq -r '.cdr_database.port' /opt/nethvoice-report.json)
   wait-for "${cdr_database_host}:${cdr_database_port}" -t 30 -- echo "cdr database is up"
 
   # Migrate cdr database
@@ -19,10 +19,10 @@ if [ "$1" = "api" ]; then
     echo "REPORTS_INTERNATIONAL_PREFIX is not set"
     exit 1
   fi
-  # Fetch credentials from conf.json
-  cdr_database_user=$(jq -r '.cdr_database.user' /opt/nethvoice-report/api/conf.json)
-  cdr_database_password=$(jq -r '.cdr_database.password' /opt/nethvoice-report/api/conf.json)
-  cdr_database_name=$(jq -r '.cdr_database.name' /opt/nethvoice-report/api/conf.json)
+  # Fetch credentials from nethvoice-report.json
+  cdr_database_user=$(jq -r '.cdr_database.user' /opt/nethvoice-report.json)
+  cdr_database_password=$(jq -r '.cdr_database.password' /opt/nethvoice-report.json)
+  cdr_database_name=$(jq -r '.cdr_database.name' /opt/nethvoice-report.json)
   # Replace $international_prefix with REPORTS_INTERNATIONAL_PREFIX
   sed \
     -e 's/@international_prefix/'"${REPORTS_INTERNATIONAL_PREFIX}"'/g' \
@@ -30,13 +30,13 @@ if [ "$1" = "api" ]; then
     /opt/nethvoice-report/scripts/migration.sql.tmpl | mysql -u"${cdr_database_user}" -p"${cdr_database_password}" -h"${cdr_database_host}" -P"${cdr_database_port}"
 
   # Wait for phonebook database to be ready
-  phonebook_database_host=$(jq -r '.phonebook_database.host' /opt/nethvoice-report/api/conf.json)
-  phonebook_database_port=$(jq -r '.phonebook_database.port' /opt/nethvoice-report/api/conf.json)
+  phonebook_database_host=$(jq -r '.phonebook_database.host' /opt/nethvoice-report.json)
+  phonebook_database_port=$(jq -r '.phonebook_database.port' /opt/nethvoice-report.json)
   wait-for "${phonebook_database_host}:${phonebook_database_port}" -t 30 -- echo "phonebook database is up"
 
   # Wait for freepbx database to be ready
-  freepbx_database_host=$(jq -r '.freepbx_database.host' /opt/nethvoice-report/api/conf.json)
-  freepbx_database_port=$(jq -r '.freepbx_database.port' /opt/nethvoice-report/api/conf.json)
+  freepbx_database_host=$(jq -r '.freepbx_database.host' /opt/nethvoice-report.json)
+  freepbx_database_port=$(jq -r '.freepbx_database.port' /opt/nethvoice-report.json)
   wait-for "${freepbx_database_host}:${freepbx_database_port}" -t 30 -- echo "freepbx database is up"
 fi
 
