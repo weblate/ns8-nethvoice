0abbbf2cac3b86e0ab1996ab94a64fa4fa7ea2ef
Stefano Fancello <stefano.fancello@nethesis.it>
2023-06-19 12:47:37 +0200

    Remove RTP range for SIP plugin, it shouldn't be needed

--- janus/entrypoint.sh
+++ janus/entrypoint.sh
@@ -11,6 +11,5 @@ if [[ -z ${LOCAL_IP} ]]; then
 	sed -i "s/\t#local_media_ip = .*/\tlocal_media_ip = \"${LOCAL_IP}\"/" /usr/local/etc/janus/janus.plugin.sip.jcfg
 fi
 sed -i "s/\tport = .*/\tport = \"${JANUS_TRANSPORT_PORT:=8089}\"/" /usr/local/etc/janus/janus.transport.http.jcfg
-sed -i "s/.*rtp_port_range = .*/\trtp_port_range = \"${ASTERISK_RTPSTART}-${ASTERISK_RTPEND}\"/" janus/usr/local/etc/janus/janus.plugin.sip.jcfg
 
 exec "$@"
