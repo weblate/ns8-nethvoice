787d636cdbce3c8bcbfdd189a3d317a28c3957e5
Stefano Fancello <stefano.fancello@nethesis.it>
2023-10-31 18:13:22 +0100

    Copy migration.php into container

--- freepbx/initdb.d/migration.php
+++ freepbx/initdb.d/migration.php
@@ -31,12 +31,12 @@ $res = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 $extensions = array_column($res, 'extension');
 
 # set media_encryption to no in freepbx sip table
-$sql = "UPDATE `asterisk`.`sip` SET `data` = 'no' WHERE `keyword` = 'media_encryption' AND `id` IN ('".str_repeat('?, ', count($extensions) - 1) . '?'."')";
+$sql = "UPDATE `asterisk`.`sip` SET `data` = 'no' WHERE `keyword` = 'media_encryption' AND `id` IN (".str_repeat('?, ', count($extensions) - 1) . '?'.")";
 $stmt = $db->prepare($sql);
 $stmt->execute($extensions);
 
 # set srtp to true in rest_devices_phones table
-$sql = "UPDATE `asterisk`.`rest_devices_phones` SET `srtp` = 1 WHERE `extension` IN ('".str_repeat('?, ', count($extensions) - 1) . '?'."')";
+$sql = "UPDATE `asterisk`.`rest_devices_phones` SET `srtp` = 1 WHERE `extension` IN (".str_repeat('?, ', count($extensions) - 1) . '?'.")";
 $stmt = $db->prepare($sql);
 $stmt->execute($extensions);
 
@@ -44,17 +44,17 @@ $stmt->execute($extensions);
 $proxy_host = $_ENV['PUBLIC_IP'];
 $proxy_port = 5060;
 
-$sql = "UPDATE `asterisk`.`sip` SET `data` = ? WHERE `keyword` = 'outbound_proxy' AND `id` IN ('".str_repeat('?, ', count($extensions) - 1) . '?'."')";
+$sql = "UPDATE `asterisk`.`sip` SET `data` = ? WHERE `keyword` = 'outbound_proxy' AND `id` IN (".str_repeat('?, ', count($extensions) - 1) . '?'.")";
 $stmt = $db->prepare($sql);
 $stmt->execute(array_merge(['sip:'.$proxy_host.':'.$proxy_port], $extensions));
 
 # set rtp_symmetric to no in freepbx sip table
-$sql = "UPDATE `asterisk`.`sip` SET `data` = 'no' WHERE `keyword` = 'rtp_symmetric' WHERE `id` IN ('".str_repeat('?, ', count($extensions) - 1) . '?'."')";
+$sql = "UPDATE `asterisk`.`sip` SET `data` = 'no' WHERE `keyword` = 'rtp_symmetric' WHERE `id` IN (".str_repeat('?, ', count($extensions) - 1) . '?'.")";
 $stmt = $db->prepare($sql);
 $stmt->execute($extensions);
 
 # set rewrite_contact to no in freepbx sip table
-$sql = "UPDATE `asterisk`.`sip` SET `data` = 'no' WHERE `keyword` = 'rewrite_contact' WHERE `id` IN ('".str_repeat('?, ', count($extensions) - 1) . '?'."')";
+$sql = "UPDATE `asterisk`.`sip` SET `data` = 'no' WHERE `keyword` = 'rewrite_contact' WHERE `id` IN (".str_repeat('?, ', count($extensions) - 1) . '?'.")";
 $db->query($sql);
 $stmt = $db->prepare($sql);
 $stmt->execute($extensions);
