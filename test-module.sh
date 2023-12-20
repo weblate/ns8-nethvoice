b0a25848e07dc64116e17e25734641aeb10c47ed
Matteo Valentini <matteo.valentini@nethesis.it>
2023-12-20 15:53:05 +0100

    test-module.sh: enable python cache

--- test-module.sh
+++ test-module.sh
@@ -13,6 +13,7 @@ ssh_key="$(cat $SSH_KEYFILE)"
 
 podman run -i \
     -v .:/home/pwuser/ns8-module:z \
+    --volume=site-packages:/home/pwuser/.local/lib/python3.8/site-packages:Z \
     --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:v10.0.3 \
     bash -l -s <<EOF
     set -e
