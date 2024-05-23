ed14182a86db95f73046d07119552a27168691ea
Matteo Valentini <matteo.valentini@nethesis.it>
2024-05-23 16:45:14 +0200

    freepbx: import of `nethserver-tancredi` repo
    Vendoring the `master` branch, starting from
    https://github.com/nethesis/nethserver-tancredi/commit/18a8a18bf0a78966f967fadf322e3c00bf5db49f

--- tancredi/usr/share/tancredi/src/Entity/NethVoiceAuth.php
+++ tancredi/usr/share/tancredi/src/Entity/NethVoiceAuth.php
@@ -60,8 +60,8 @@ class NethVoiceAuth
             }
         } elseif ($request->hasHeader('Secretkey') && $request->hasHeader('User')) {
             $dbh = new \PDO(
-                'mysql:dbname=asterisk;host=localhost',
-                'tancredi',
+                'mysql:dbname=asterisk;host='.($this->config['auth_nethvoice_dbhost'] ?? 'localhost').';port='.($this->config['auth_nethvoice_dbport'] ?? '3306'),
+                ($this->config['auth_nethvoice_dbuser'] ?? 'tancredi'),
                 $this->config['auth_nethvoice_dbpass']
             );
             $stmt = $dbh->prepare("SELECT * FROM ampusers WHERE sections LIKE '%*%' AND username = ?");
