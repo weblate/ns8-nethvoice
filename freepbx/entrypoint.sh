ec526ebbaf15348178c31a5e2e06a4b3e0aa76a3
Stefano <gentoo.stefano@gmail.com>
2024-07-24 09:23:55 +0200

    Set the from address and the mail command (#268)
    * Set the from address and the mail command
    
    * adjust the domain guessing
    
    * use the already setted variables also for smtps
    
    * use sed to encode characters not correctly encoded by jq
    
    * Use bash function to encode username and password
    
    * simplify logic
    
    * Don't send duplicate Date: header
    
    Asterisk writes the "Date:" header into email bdy, but s-nail writes it again. It isn't possible to disable it from configuration in voicemail.conf or s-nail.rc, therefore here there is a simple bash script that removes the Date: header then pipes the mail to s-nail. It is possible to improve the script further in future, for instance by sending the voicemail to other channels or do STT on the attachment.
    
    * Make it work if SMTP configuration is "use local mail app"

--- freepbx/entrypoint.sh
+++ freepbx/entrypoint.sh
@@ -227,24 +227,89 @@ source /etc/apache2/envvars
 # Install freepbx modules and apply changes after asterisk is started by supervisor
 /freepbx_init.sh &
 
+# bash function to URL-encode a string
+url_encode() {
+    local input_string="$1"
+    local encoded_string
+
+    # URL-encode the input string using jq
+    encoded_string=$(printf %s "${input_string}" | jq -sRr @uri)
+
+    # Perform specific replacements for certain characters
+    encoded_string=$(echo "${encoded_string}" | sed 's/!/%21/g;s/*/%2A/g;s/(/%28/g;s/)/%29/g;s/'"'"'/%27/g')
+
+    echo "${encoded_string}"
+}
+
 # Configure SMTP for Voicemail
 if [ "$SMTP_ENABLED" = "1" ]; then
-	cat <<EOF >> /etc/s-nail.rc
-set smtp-auth=login
-set tls-verify=$(if [ "$SMTP_TLSVERIFY" = "1" ]; then echo "strict"; else echo "ignore"; fi)
-set v15-compat=yes
-EOF
+	# Write s-nail configuration
 
-	# Check if encryption is specified and modify configuration accordingly
-	USER_ENCODED_STRING=$(printf %s "${SMTP_USERNAME}"|jq -sRr @uri)
-	PASSWORD_ENCODED_STRING=$(printf %s "${SMTP_PASSWORD}"|jq -sRr @uri)
-	if [ "$SMTP_ENCRYPTION" = "starttls" ]; then
-		echo "set smtp-use-starttls" >> /etc/s-nail.rc
-		echo "set mta=smtp://${USER_ENCODED_STRING}:${PASSWORD_ENCODED_STRING}@${SMTP_HOST}:${SMTP_PORT}" >> /etc/s-nail.rc
-	elif [ "$SMTP_ENCRYPTION" = "tls" ]; then
-		echo "set mta=smtps://$(printf %s "${SMTP_USERNAME}"|jq -sRr @uri):$(printf %s "${SMTP_PASSWORD}"|jq -sRr @uri)@${SMTP_HOST}:${SMTP_PORT}" >> /etc/s-nail.rc
+	# Set the s-nail compatibility mode
+	echo "set v15-compat=yes" >> /etc/s-nail.rc
+
+	# Set the TLS verify policy
+	if [ "$SMTP_TLSVERIFY" = "1" ]; then
+		echo "set tls-verify=strict" >> /etc/s-nail.rc
+	else
+		echo "set tls-verify=ignore" >> /etc/s-nail.rc
+	fi
+
+	# Set the authentication method
+	if [ -z "$SMTP_PASSWORD" ]; then
+		echo "set smtp-auth=none" >> /etc/s-nail.rc
+		# Set the SMTP server URL without authentication
+		echo "set mta=smtp://${SMTP_HOST}:${SMTP_PORT}" >> /etc/s-nail.rc
+	else
+		echo "set smtp-auth=login" >> /etc/s-nail.rc
+		# Set the SMTP server URL with authentication
+		# Check if encryption is specified and modify URL accordingly
+		if [ "$SMTP_ENCRYPTION" = "starttls" ]; then
+			# use starttls for STARTTLS encryption
+			echo "set smtp-use-starttls" >> /etc/s-nail.rc
+			echo "set mta=smtp://$(url_encode "${SMTP_USERNAME}"):$(url_encode "${SMTP_PASSWORD}")@${SMTP_HOST}:${SMTP_PORT}" >> /etc/s-nail.rc
+		elif [ "$SMTP_ENCRYPTION" = "tls" ]; then
+			# use smtps:// for TLS encryption
+			echo "set mta=smtps://$(url_encode "${SMTP_USERNAME}"):$(url_encode "${SMTP_PASSWORD}")@${SMTP_HOST}:${SMTP_PORT}" >> /etc/s-nail.rc
+		else
+			# default to no encryption
+			echo "set mta=smtp://${SMTP_USERNAME}:$(url_encode "${SMTP_PASSWORD}")@${SMTP_HOST}:${SMTP_PORT}" >> /etc/s-nail.rc
+		fi
+	fi
+
+	# Set the mailcmd
+	if ! grep -q '^mailcmd=' /etc/asterisk/voicemail.conf; then
+		# write mailcmd if it isn't already set
+		sed -i "s|^\[general\]$|[general]\nmailcmd=/var/lib/asterisk/bin/send_email|" /etc/asterisk/voicemail.conf
+	elif grep -q '^mailcmd=/usr/sbin/sendmail' /etc/asterisk/voicemail.conf; then
+		# replace mailcmd if it is already set and is the old binary
+		sed -i "s|^mailcmd=/usr/sbin/sendmail.*|mailcmd=/var/lib/asterisk/bin/send_email|" /etc/asterisk/voicemail.conf
+	fi
+	# set the from address
+	if [ -z "$SMTP_FROM_ADDRESS" ]; then
+		if echo "$SMTP_USERNAME" | grep -q '@'; then
+			# get the from address from the smtp username
+			FROM_DOMAIN=$(echo "$SMTP_USERNAME" | cut -d'@' -f2)
+		elif echo "$SMTP_HOST" | grep -q '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*'; then
+			# get the from address from NETHVOICE_HOST if smtp host is an IP address
+			FROM_DOMAIN=$(echo "$NETHVOICE_HOST" | cut -d'.' -f2-)
+		else
+			# get the from address from the smtp host
+			FROM_DOMAIN=$(echo "$SMTP_HOST" | cut -d'.' -f2-)
+		fi
+		FROM_NAME=$(echo "${BRAND_NAME}" | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g')
+		SMTP_FROM_ADDRESS="${FROM_NAME}@${FROM_DOMAIN}"
+	fi
+
+	# add the from address to the s-nail configuration
+	echo "set from=${SMTP_FROM_ADDRESS}" >> /etc/s-nail.rc
+
+	# set the email from address in voicemail configuration if it isn't already set from FreePBX interface
+	if ! grep -q '^serveremail *=' /etc/asterisk/voicemail.conf; then
+		sed -i "s/^\[general\]$/[general]\nserveremail=${SMTP_FROM_ADDRESS}/" /etc/asterisk/voicemail.conf
 	fi
 fi
+
 # customize voicemail branding
 sed 's/FreePBX/'"${BRAND_NAME}"'/' -i /etc/asterisk/voicemail.conf*
 sed 's/http:\/\/AMPWEBADDRESS\/ucp/https:\/\/'"${NETHCTI_UI_HOST}"'\/history/' -i /etc/asterisk/voicemail.conf*
