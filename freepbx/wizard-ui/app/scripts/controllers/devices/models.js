8c4589204876afe34dfa4ea674dfeab06c3731a2
Antonio <36625268+tonyco97@users.noreply.github.com>
2024-09-19 12:45:54 +0200

    models. Fixed missing custom models inside list (#300)

--- freepbx/wizard-ui/app/scripts/controllers/devices/models.js
+++ freepbx/wizard-ui/app/scripts/controllers/devices/models.js
@@ -38,6 +38,7 @@ angular.module('nethvoiceWizardUiApp')
           for (let model in res.data) {
             if (res.data[model].name.split("-").length > 2
                 && !$scope.inventoryModels.find(function (el) { return el.name === res.data[model].name; })) {
+              $scope.inventoryModels.push(res.data[model])
             }
           }
         }, function (err) {
