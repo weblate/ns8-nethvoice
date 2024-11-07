f7a641684f984bf8f4478047b6873a0b91e1761f
Andrea Marchionni <andrea.marchionni@nethesis.it>
2024-11-07 16:41:08 +0100

    feat: add Nethesis NP-V61 support (#332)
    - Added support for Nethesis NP-V61 model
    - Related issue: https://github.com/NethServer/dev/issues/7032

--- freepbx/wizard-ui/app/scripts/controllers/devices/inventory.js
+++ freepbx/wizard-ui/app/scripts/controllers/devices/inventory.js
@@ -31,9 +31,11 @@ angular.module('nethvoiceWizardUiApp')
     const npx3Digits = "00"
     const npx5Digits = "01"
     const npx210Digits = "02"
+    const npv61Digits = "03"  
     const npx3Name = "nethesis-NPX3"
     const npx5Name = "nethesis-NPX5"
     const npx210Name = "nethesis-NPX210"
+    const npv61Name = "nethesis-NPV61"
 
     function gotModels(models) {
       $scope.models = models;
@@ -240,7 +242,9 @@ angular.module('nethvoiceWizardUiApp')
             $scope.manualModel = $scope.manualFilteredModels.find(( model ) => { return model.name === npx5Name })
           } else if (mac.toUpperCase().split("-")[modelDigitsKey] === npx210Digits) {
             $scope.manualModel = $scope.manualFilteredModels.find(( model ) => { return model.name === npx210Name })
-          }
+          } else if (mac.toUpperCase().split("-")[modelDigitsKey] === npv61Digits) {
+            $scope.manualModel = $scope.manualFilteredModels.find(( model ) => { return model.name === npv61Name })		  
+          }		  
         }
       } else {
         $scope.manualVendor = null;
@@ -694,6 +698,8 @@ angular.module('nethvoiceWizardUiApp')
             phone.model = phone.filteredModels.find(( model ) => { return model.name === npx5Name })
           } else if (mac.toUpperCase().split("-")[modelDigitsKey] === npx210Digits) {
             phone.model = phone.filteredModels.find(( model ) => { return model.name === npx210Name })
+          } else if (mac.toUpperCase().split("-")[modelDigitsKey] === npv61Digits) {
+            phone.model = phone.filteredModels.find(( model ) => { return model.name === npv61Name })
           }
         }
       } else {
