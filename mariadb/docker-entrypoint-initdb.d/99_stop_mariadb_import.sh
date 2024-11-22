40143a359b0a1e1dc24d425cc2e4a42d9f4cfac4
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-11-22 13:55:17 +0100

    Update mariadb/docker-entrypoint-initdb.d/99_stop_mariadb_import.sh
    Co-authored-by: Matteo Valentini <matteo.valentini@nethesis.it>

--- mariadb/docker-entrypoint-initdb.d/99_stop_mariadb_import.sh
+++ mariadb/docker-entrypoint-initdb.d/99_stop_mariadb_import.sh
@@ -1,5 +1,10 @@
 # Prepare the script that stops the container
 # The .sh file is sourced by the Bash entrypoint script
 # because it is not an executable file:
+mysql_note "Stopping temporary server"
 docker_temp_server_stop # function defined by the entrypoint script
+mysql_note "Temporary server stopped"
+echo
+mysql_note "MariaDB init process done. Ready for start up."
+echo
 exit 0 # exit the entrypoint immediately: do not start the real DB server
